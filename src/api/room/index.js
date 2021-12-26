2dd98c346118dddc2bfdef204be928090d129bcd
hirazy <nangle123456@gmail.com>
2021-12-27 03:02:08 +0700

    update --

--- src/api/room/index.js
+++ src/api/room/index.js
@@ -53,7 +53,7 @@ router.get('/:id',
  * @apiError {Object} 400 Some parameters may contain invalid values.
  * @apiError 404 Some entity not found.
  */
-router.put('/:id/message',
+router.put('/message/:id',
     master(),
     body({ content, senderID, receiverID, isImage }),
     add_message)
