bc534a196f471cb4f121ebdf53b39226e0823184
hirazy <nangle123456@gmail.com>
2022-01-21 03:27:27 +0700

    update --

--- src/api/image/index.js
+++ src/api/image/index.js
@@ -9,7 +9,7 @@ var storage = multer.diskStorage({
     destination: function(req, file, cb) {
 
         // Uploads is the Upload_folder_name
-        cb(null, "./uploads")
+        cb(null, "uploads")
     },
     filename: function(req, file, cb) {
         cb(null, file.fieldname + "-" + Date.now() + ".jpg")
