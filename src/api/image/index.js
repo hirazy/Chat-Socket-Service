dc1fa1501e14c6d168b46cfde39a5b978136972a
hirazy <nangle123456@gmail.com>
2022-01-22 03:27:47 +0700

    update --

--- src/api/image/index.js
+++ src/api/image/index.js
@@ -2,12 +2,24 @@ import { Router } from 'express'
 import { success, notFound } from '../../services/response/'
 import { password, master } from '../../services/passport'
 
+const fs = require("fs");
 const multer = require('multer');
-const upload = require('../uploadMiddleware');
-const Resize = require('../Resize');
+// const upload = require('../uploadMiddleware');
+// const Resize = require('../Resize');
 const path = require('path');
 
 const router = new Router()
+
+var multer = require('multer')
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function(req, file, callback) {
+        callback(null, Date.now() + '-' + file.originalname);
+    }
+});
+var upload = multer({ storage: storage });
 
 // var storage = multer.diskStorage({
 //     destination: function(req, file, cb) {
@@ -46,6 +58,51 @@ const router = new Router()
 
 //     // mypic is the name of file attribute
 // }).single("mypic");
+/**
+ * @api {post} /image Upload image
+ * @apiName Upload Image
+ * @apiGroup Image
+ * @apiPermission master
+ * @apiHeader {String} Upload image to folder /uploads 
+ * @apiParam {String} access_token Master access_token.
+ * @apiSuccess (Success 201) {String} name of saved file to be passed to other requests.
+ * @apiSuccess (Success 201) {Object} user Current user's data.
+ * @apiError 401 Master access only or invalid credentials.
+ */
+router.get('/', master(), (req, res) => {
+    var action = req.body.path;
+
+    var filePath = path.join(__dirname, "/uploads/" + action).split("%20").join(" ");
+
+    // Checking if the path exists
+    fs.exists(filePath, function(exists) {
+        if (!exists) {
+            res.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            res.end("404 Not Found");
+            return;
+        }
+        // Extracting file extension
+        var ext = path.extname(action);
+        // Setting default Content-Type
+        var contentType = "text/plain";
+        // Checking if the extension of
+        // image is '.png'
+        if (ext === ".png") {
+            contentType = "image/png";
+        }
+        // Setting the headers
+        res.writeHead(200, {
+            "Content-Type": contentType
+        });
+        // Reading the file
+        fs.readFile(filePath, (err, content) => {
+            // Serving the image
+            res.end(content);
+        });
+    });
+})
 
 /**
  * @api {post} /image Upload image
@@ -60,9 +117,9 @@ const router = new Router()
  */
 router.post('/', master(), upload.single('image'), async(req, res) => {
 
-    const imagePath = path.join(__dirname, '/uploads');
-    // call class Resize
-    const fileUpload = new Resize(imagePath);
+    // const imagePath = path.join(__dirname, '/uploads');
+    // // call class Resize
+    // const fileUpload = new Resize(imagePath);
     if (!req.file) {
         res.status(401).json({ error: 'Please provide an image' });
     } else {
