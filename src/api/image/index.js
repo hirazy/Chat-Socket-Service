095bf24f6d204d84101de03f09e2ac42afac7552
hirazy <nangle123456@gmail.com>
2022-01-21 13:58:44 +0700

    update --

--- src/api/image/index.js
+++ src/api/image/index.js
@@ -3,45 +3,48 @@ import { success, notFound } from '../../services/response/'
 import { password, master } from '../../services/passport'
 
 const multer = require('multer');
+const upload = require('../uploadMiddleware');
+const Resize = require('../Resize');
+
 const router = new Router()
 
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+// var storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
 
-        // Uploads is the Upload_folder_name
-        cb(null, '/uploads')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + "-" + Date.now() + ".jpg")
-    }
-})
+//         // Uploads is the Upload_folder_name
+//         cb(null, '/uploads')
+//     },
+//     filename: function(req, file, cb) {
+//         cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+//     }
+// })
 
-// Define the maximum size for uploading
-// picture i.e. 1 MB. it is optional
-const maxSize = 1 * 1000 * 1000;
+// // Define the maximum size for uploading
+// // picture i.e. 1 MB. it is optional
+// const maxSize = 1 * 1000 * 1000;
 
-var upload = multer({
-    storage: storage,
-    limits: { fileSize: maxSize },
-    fileFilter: function(req, file, cb) {
+// var upload = multer({
+//     storage: storage,
+//     limits: { fileSize: maxSize },
+//     fileFilter: function(req, file, cb) {
 
-        // Set the filetypes, it is optional
-        var filetypes = /jpeg|jpg|png/;
-        var mimetype = filetypes.test(file.mimetype);
+//         // Set the filetypes, it is optional
+//         var filetypes = /jpeg|jpg|png/;
+//         var mimetype = filetypes.test(file.mimetype);
 
-        var extname = filetypes.test(path.extname(
-            file.originalname).toLowerCase());
+//         var extname = filetypes.test(path.extname(
+//             file.originalname).toLowerCase());
 
-        if (mimetype && extname) {
-            return cb(null, true);
-        }
+//         if (mimetype && extname) {
+//             return cb(null, true);
+//         }
 
-        cb("Error: File upload only supports the " +
-            "following filetypes - " + filetypes);
-    }
+//         cb("Error: File upload only supports the " +
+//             "following filetypes - " + filetypes);
+//     }
 
-    // mypic is the name of file attribute
-}).single("mypic");
+//     // mypic is the name of file attribute
+// }).single("mypic");
 
 /**
  * @api {post} /image Upload image
@@ -54,21 +57,17 @@ var upload = multer({
  * @apiSuccess (Success 201) {Object} user Current user's data.
  * @apiError 401 Master access only or invalid credentials.
  */
-router.post('/', master(), (req, res) => {
+router.post('/', master(), upload.single('image'), (req, res) => {
 
-    upload(req, res, function(err) {
+    const imagePath = path.join(__dirname, '/uploads');
+    // call class Resize
+    const fileUpload = new Resize(imagePath);
+    if (!req.file) {
+        res.status(401).json({ error: 'Please provide an image' });
+    }
+    const filename = await fileUpload.save(req.file.buffer);
 
-        console.log(err)
-
-        if (err) {
-            notFound(res)
-        } else {
-
-            // SUCCESS, image successfully uploaded
-            success(res, 201)
-        }
-    })
-
+    res.status(200).json({ name: filename });
 })
 
 export default router
