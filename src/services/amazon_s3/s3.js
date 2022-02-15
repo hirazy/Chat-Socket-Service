5954d8f034bfb11d733c2b522d83066b59caddc8
hirazy <nangle123456@gmail.com>
2022-02-15 23:07:34 +0700

    update --

--- src/services/amazon_s3/s3.js
+++ src/services/amazon_s3/s3.js
@@ -12,7 +12,7 @@ const s3 = new S3({
 
 // upload a file to s3
 export function uploadFile(path, name) {
-    console.log("AWS_BUCKET_NAME " + awsBucketName)
+    console.log("AWS " + awsBucketName + " " + awsAccessKey + " " + awsSecretKey)
 
     const fileStream = fs.createReadStream(path)
 
