6a4c5f7153ceec77aa20c9dde06dd849060e0893
hirazy <nangle123456@gmail.com>
2022-02-15 17:34:16 +0700

    update --

--- src/services/amazon_s3/s3.js
+++ src/services/amazon_s3/s3.js
@@ -7,7 +7,7 @@ const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_KEY
 
-const s3 = S3({
+const s3 = new S3({
     region,
     accessKeyId,
     secretAccessKey
