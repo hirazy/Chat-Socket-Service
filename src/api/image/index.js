5c4a12dfce0630a74e644fd4e8d8c5e9209b62ba
hirazy <nangle123456@gmail.com>
2022-02-17 02:52:06 +0700

    update --

--- src/api/image/index.js
+++ src/api/image/index.js
@@ -81,7 +81,7 @@ router.post('/', master(), upload.single('image'), async(req, res) => {
 
     const file = req.file
 
-    console.log(file['mimetype'])
+    console.log(file['type'])
 
     if (!file || file['mimetype'].split('/')[0] !== 'image') {
         res.status(401).json({ error: 'Please provide an image' });
