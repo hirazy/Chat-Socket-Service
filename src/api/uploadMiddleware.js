430d2ee68e3ea3fab73d9433ba4755429f37a59e
hirazy <nangle123456@gmail.com>
2022-02-16 23:37:01 +0700

    update --

--- src/api/uploadMiddleware.js
+++ src/api/uploadMiddleware.js
@@ -1,9 +1,5 @@
 const multer = require('multer');
 
-const upload = multer({
-    limits: {
-        fileSize: 4 * 1024 * 1024,
-    }
-});
+const upload = multer({ dest: 'uploads/' })
 
 module.exports = upload
