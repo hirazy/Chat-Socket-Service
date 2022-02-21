2c233fd05cc6f7abf00c3aae8bc1017d924663ee
hirazy <nangle123456@gmail.com>
2022-02-22 03:27:34 +0700

    update --

--- src/api/password-reset/controller.js
+++ src/api/password-reset/controller.js
@@ -14,8 +14,11 @@ export const create = ({ bodymen: { body: { email, link } } }, res, next) =>
         const content = `
         Hey, ${user.name}.<br><br>
         You requested a new password for your Server Chatapp account.<br>
-        Please use the following link to set a new password. It will expire in 1 hour.<br><br>
-        <a href="${link}">Click here to reset password.</a><br><br>
+        Please use the following link to set a new password. It will expire in 1 hour.
+        <br><br>
+        Click <a href="${token}">${token}</a> to copy to clipboard and reset password.
+        </br></br>
+
         If you didn't make this request then you can safely ignore this email. :)<br><br>
         &mdash; Server Chatapp Team
       `
@@ -23,6 +26,9 @@ export const create = ({ bodymen: { body: { email, link } } }, res, next) =>
     })
     .then(([response]) => response ? res.status(response.statusCode).end() : null)
     .catch(next)
+
+// <br><br>
+// <a href="${link}">Click here to reset password.</a><br><br>
 
 export const show = ({ params: { token } }, res, next) =>
     PasswordReset.findOne({ token })
